81aff18d92388cd84a1dbf8fea916dee737fe2ab
Olivia <oliviaflexx@gmail.com>
2021-09-25 23:06:41 -0400

    change to pic if screen small

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,20 +1,35 @@
 import React from "react";
-import { Video } from "cloudinary-react";
+import { Video, Image } from "cloudinary-react";
 import Footer from "./Footer";
 
 function Home() {
-    return (
-      <React.Fragment>
-        <main className="home">
-          <section className="video-container" id="render-here">
-            <Video
+  const [background, setBackground] = React.useState(<Video
               cloudName="www-craigfelix-com"
               publicId="aerial_footage/3"
               loop={true}
               autoPlay={true}
               sourceTypes="mov"
               muted
-            />
+            />);
+
+  function checkMatch(x) {
+    if (x.matches) {
+      // If media query matches
+      setBackground(<Image cloudName="www-craigfelix-com" publicId="st_lucia17" />);
+    }
+  }
+
+    React.useEffect(() => {
+       var x = window.matchMedia("(max-width: 480px)");
+       console.log(x);
+       checkMatch(x);
+    }, []);
+
+    return (
+      <React.Fragment>
+        <main className="home">
+          <section className="video-container" id="render-here">
+            {background}
             <p id="main-quote">See the world through Craig's eyes</p>
             <div class="bottom-fade"></div>
           </section>
