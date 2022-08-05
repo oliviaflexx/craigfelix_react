62710eba49f1b32fde159884a82260f67e0c2f3c
Olivia <oliviaflexx@gmail.com>
2022-08-05 14:57:01 -0500

    about text change

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -11,20 +11,15 @@ function Contact() {
             <h2>About</h2>
             <p>
               <div className="non-personal-text">
-                Growing up on the island of St. Lucia, I was surrounded by
-                tremendous natural beauty, a fact that I didn’t fully appreciate
-                until after I had moved to the USA and returned to St.Lucia on a
-                yearly basis. I discovered photography as a hobby at that time
-                and was inspired to become a professional photographer. I
-                immensely enjoy travel photography using natural light and
-                perspective to capture the beauty of a scene and to engage my
-                viewers’ interest. Beauty is all around us. We can travel
-                thousands of miles to look for it or find it inches wherever we
-                are. For fun I choose to go further. I have over 17 years of
-                experience as commercial product photographer . I have a BA in
-                Economics and I am also an FAA -Certified Drone Pilot. My love
-                of travel has greatly inspired my photography I am excited to
-                share my images with you.
+                I have had over 15 years of experience as a full-time commercial
+                photographer for the largest department store retailer in the
+                USA. There, I produced high quality work for promotional
+                advertising purposes. I am able to excel under pressure while
+                meeting strict deadlines to create attractive photography for
+                all types of media. I am also an FAA -Certified Drone Pilot.
+                Prior to my career in photography, I earned a BA in Economics
+                with a minor in marketing from Marquette University. My passion
+                is to capture striking, memorable imagery wherever it may be.
               </div>
             </p>
             <Image cloudName="www-craigfelix-com" publicId={"about"} />
